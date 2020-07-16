3c6fe51266045dd4ab3c0f3d60d26c63433c07f2
TheUcheojor <okennepaul@gmail.com>
2020-07-16 04:52:43 -0400

    Setup Overview Page Layout

--- stocky-frontend/src/pages/OverviewPage.js
+++ stocky-frontend/src/pages/OverviewPage.js
@@ -3,10 +3,17 @@ import React from 'react'
 const OverviewPage=()=>(
 
     <div id='content'> 
-         <h1>This is the OverviewPage</h1>
+        <div className='container account-details'> Account Details </div>
+        <div className='container daily-profit'> Daily Profit </div>
+        <div className='container live-overview'> Live Overview</div>
+        <div className='container api-info'> Alpaca API Info</div>
+        <div className='container order-history'> Order History </div>
+        <div className='container manual-order'> Manual Order </div>
     
     </div>
 
 )
-
+// "account-details .  daily-profit"
+// "live-overview live-overview api-info"
+// "order-history order-history manual-order"
 export default OverviewPage;
