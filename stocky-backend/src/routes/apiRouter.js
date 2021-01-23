81f3fc286978b1853143dbb3b9ae51708969dcf5
Paul Okenne <paulokenne@cmail.carleton.ca>
2021-01-22 22:44:31 -0500

    feat: implement more api routes

--- stocky-backend/src/routes/apiRouter.js
+++ stocky-backend/src/routes/apiRouter.js
@@ -1,5 +1,8 @@
 import express from 'express'
 
+import async from 'async'
+import axios from 'axios'
+
 import Alpaca from '@alpacahq/alpaca-trade-api'
 import MeanReversion from '../trading/strategies/mean-reversion'
 import keys from '../config/setup'
@@ -7,36 +10,137 @@ import User from '../models/User'
 
 import connectToAlpaca from '../auxilliary/connectToAlpaca'
 
+
 const apiRouter = express.Router()
 
 
+apiRouter.get('/user-portfolio', (req,res)=>{
+
+    const { email }= req.body
+
+    User.findOne({email:email})
+    .then( (user)=>{
 
 
+        const alpaca=connectToAlpaca(user)
 
+        console.log("Getting Positions..")
+        alpaca.getPositions()
+        .then((portfolio) => {
 
-// apiRouter.get('/stocks/:name',(req,res)=>{
+            let portfolioDetails={};
 
-//     const { email }= req.body
-//     const name=req.params.name
+            // Print the quantity of shares for each position.
+            portfolio.forEach(function (position) {
+                portfolioDetails[position.symbol]=position.qty
+            })
 
-//     User.findOne({email:email})
-//     .then( (user)=>{
+            res.status(200).json({success:true, data:portfolioDetails})
 
-//         const alpaca=connectToAlpaca(user)
+        }).catch(err=>{
+            console.log(err)
+            res.status(500).json({success:false,message:'Cannot get portfolio'})
+        })
 
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).json({success:false,message:' Account does not exist'})
+    })
+    
+    
+})
 
+apiRouter.get('/get-order-history', (req,res)=>{
 
+    const { email } = req.body;
+
+    User.findOne({email:email})
+    .then((user)=>{
         
+        const alpaca=connectToAlpaca(user)
 
-        
-//     }).catch(err=>{
-//         console.log(err)
-//         res.status(500).json({success:false,message:' Account does not exist'})
-//     })
+        const MAX_ACTIVITIES= 10;
+        // { activityTypes, until, after, direction, date, pageSize }: 
+        // { activityTypes: any; until: any; after: any; direction: any; date: any; pageSize: any; }
+       
+        alpaca.getAccountActivities({activityTypes:["FILL"], pageSize: MAX_ACTIVITIES})
+        .then( (activities)=>{
+            
+            let formattedActivities=[];
+
+            async.each(activities, function(activity, callback){
+
+                alpaca.getOrder(activity["order_id"])
+                .then((order)=>{
+                    
+                    formattedActivities.push(
+                        {
+                            symbol: activity.symbol,
+                            type: activity.type,
+                            quantity:activity.quantity,
+                            price:activity.price,
+                            transaction_time:activity.transaction_time,
+                            order_type:order.type,
+                            status: order.status
+                        }
+                    )
+
+                    callback()
+
+                }).catch(error=>{
+                    console.log(`error: ${error}`)
+                    res.status(500).json({success:false, message:`${error}`})
+                })
+
+
+            }  ).then( ()=>{
+
+                res.status(200).json({success:true, data:formattedActivities})
+
+            }).catch(err=>{
+                res.status(500).json({success:false, message:`${err}`})
+            })
+            
+
+
+
+        }).catch( (error)=>{
+            console.log(`error: ${error}`)
+
+            res.status(500).json({success:false, message:`${error}`})
+        })
+
+
+    }).catch( (error)=>{
+
+        res.status(500).json({success:false, message:"No User associated with the given email"})
+
+    } )
+
+
+
+});
+
+
+
+apiRouter.get('/stocks/:name',(req,res)=>{
+
+    const name=req.params.name
+
+    console.log(name)
+    console.log(`https://kgsearch.googleapis.com/v1/entities:search?query=${name}&key=${keys.SEARCH_API_KEY}&limit=1&indent=True`)
+    
+    axios.get(`https://kgsearch.googleapis.com/v1/entities:search?query=${name}&key=${keys.SEARCH_API_KEY}&limit=1&indent=True`)
+    .then(axiosResponse=>{
+        console.log(axiosResponse.data)
+        res.status(200).json({success:true, data: axiosResponse.data.itemListElement[0] })
+    }).catch(err=>{
+        res.status(500).json({success:false, message:`${err}`})
+    })
     
 
     
-// })
+})
 
 
 
--- stocky-backend/src/routes/settingsRouter.js
+++ stocky-backend/src/routes/settingsRouter.js
@@ -1,6 +1,49 @@
 import express from 'express'
+import User from '../models/User'
 
 const settingsRouter = express.Router()
+
+
+settingsRouter.get("/set-password",(req,res)=>{
+
+    const {email, password}= req.body;
+
+
+    User.updateOne( {email: email}, {$set: {password: password}})
+
+    .then( ()=>res.status(200).json( {success:true, data:email}) )
+
+})
+
+
+settingsRouter.get("/set-alpaca",(req,res)=>{
+
+    const {email, secretKey, apiKey}= req.body;
+
+    User.updateOne( {email: email}, {$set: {
+        "alpaca.secretKey": secretKey,
+        "alpaca.apiKey": apiKey
+    
+    }})
+
+    .then( ()=>res.status(200).json( {success:true, data:email}) )
+
+})
+
+settingsRouter.get("/set-strategy",(req,res)=>{
+
+    const {email, strategy}= req.body;
+
+    //COME BACK TO THIS....Thinking of how to integrate background tasks
+    User.updateOne( {email: email}, {$set: {
+        "settings.strategy": secretKey,
+    }})
+
+    .then( ()=>res.status(200).json( {success:true, data:email}) )
+
+})
+
+
 
 
 export default settingsRouter
