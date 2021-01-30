2b084b02d7b2467308efcbd01828de2ad56238fc
Paul Okenne <paulokenne@cmail.carleton.ca>
2021-01-30 01:56:16 -0500

    refact: comment out log

--- stocky-backend/src/auxilliary/connectToAlpaca.js
+++ stocky-backend/src/auxilliary/connectToAlpaca.js
@@ -2,6 +2,7 @@ import Alpaca from '@alpacahq/alpaca-trade-api'
 
 const connectToAlpaca=(user)=>{
 
+    // console.log(`\n connectToAlpaca - user: ${JSON.stringify(user)}`)
     const userStockyConfig={
         keyId:user.alpaca.apiKey,
         secretKey:user.alpaca.secretKey,
