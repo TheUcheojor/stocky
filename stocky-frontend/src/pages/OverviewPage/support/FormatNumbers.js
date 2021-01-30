8d55092d022e87177a7b31d12a793e1384c34206
Paul Okenne <paulokenne@cmail.carleton.ca>
2021-01-30 02:05:59 -0500

    fix: address issues when formating numbers

--- stocky-frontend/src/pages/OverviewPage/support/FormatNumbers.js
+++ stocky-frontend/src/pages/OverviewPage/support/FormatNumbers.js
@@ -1,27 +1,33 @@
 
-const formatNumber=(num)=>{
+const CASH_SYMBOL="$";
+const NEGATIVE_SIGN="-";
 
+const formatNumber=(num)=>{
 
     if(!num) return num;
 
     let numString=JSON.stringify(num)
 
-    let rawDecimals=numString.split(".")[1]
+    let rawDecimals=(numString.split(".")[1])? numString.split(".")[1]:""
     let rawIntegers=numString.split(".")[0]
+
+    let sign="";
+    if( rawIntegers.includes(NEGATIVE_SIGN) ){
+        sign=NEGATIVE_SIGN
+        rawIntegers=rawIntegers.split(NEGATIVE_SIGN)[1]
+    }
     
     let formattedIntegers=""
 
     for(var i =0; i<rawIntegers.length; i++){
-
         if( (i+1)%3==0 ){
             formattedIntegers+=rawIntegers[i]+", "
         }else{
             formattedIntegers+=rawIntegers[i]
         }
-
     }
 
-    return (formattedIntegers+rawDecimals).slice(1, -1)
+    return CASH_SYMBOL+(sign+formattedIntegers+rawDecimals).slice(1, -1)
 
 
 }
