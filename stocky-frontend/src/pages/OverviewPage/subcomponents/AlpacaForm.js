52d8289829ce79b6007add6ee43c197efb30b7df
Paul Okenne <okennepaul@gmail.com>
2021-02-02 19:46:48 -0500

    feat: make alpaca form glow red for empty and invalid keys

--- stocky-frontend/src/pages/OverviewPage/subcomponents/AlpacaForm.js
+++ stocky-frontend/src/pages/OverviewPage/subcomponents/AlpacaForm.js
@@ -1,33 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import saveAplacaSettings from '../support/saveAlpacaSetting'
 
 
-const AlpacaForm= ({email,apiKey,secretKey,setApiKey,setSecretKey})=>(
+const AlpacaForm= ({email,apiKey,secretKey,setApiKey,setSecretKey})=>{
+    
+    useEffect( ()=>{
 
-    <div className='container alpaca-form api-info'> 
-        <span className="container-title">Alpaca API Info</span>
-        <div style={{paddingTop:"5px"}} >
+        if(apiKey=="" || secretKey==""){
+            document.getElementsByClassName("alpaca-form")[0].classList.add("glow")
+        }else{
+            document.getElementsByClassName("alpaca-form")[0].classList.remove("glow")
+        }
+    },[apiKey,secretKey])
+    
+    
+    return (
 
-            <span className="aplaca-info-title" >Secret Key</span>
-            <input type="text" className="aplaca-info" id="secretKey" placeholder={secretKey}></input>
+        <div className='container alpaca-form api-info'> 
+            <span className="container-title">Alpaca API Info</span>
+            <div style={{paddingTop:"5px"}} >
 
-            <span className="aplaca-info-title">Api Key</span>
-            <input type="text" className="aplaca-info" id="apiKey" placeholder={apiKey}></input>
-                        
-            <button id="saveApiSettings" 
-                onClick={()=>saveAplacaSettings(
-                                {
-                                    email:email, 
-                                    apiKey:(document.getElementById('apiKey').value)? document.getElementById('apiKey').value: apiKey, 
-                                    secretKey:(document.getElementById('secretKey').value)? document.getElementById('secretKey').value:secretKey,
-                                    setApiKey:setApiKey, 
-                                    setSecretKey:setSecretKey, 
-                })}>
-                    Save
-                </button>
+                <span className="aplaca-info-title" >Secret Key</span>
+                <input type="text" className="aplaca-info" id="secretKey" placeholder={secretKey}></input>
 
+                <span className="aplaca-info-title">Api Key</span>
+                <input type="text" className="aplaca-info" id="apiKey" placeholder={apiKey}></input>
+                            
+                <button id="saveApiSettings" 
+                    onClick={()=>saveAplacaSettings(
+                                    {
+                                        email:email, 
+                                        apiKey:(document.getElementById('apiKey').value)? document.getElementById('apiKey').value: apiKey, 
+                                        secretKey:(document.getElementById('secretKey').value)? document.getElementById('secretKey').value:secretKey,
+                                        setApiKey:setApiKey, 
+                                        setSecretKey:setSecretKey, 
+                    })}>
+                        Save
+                    </button>
+
+            </div>
         </div>
-    </div>
-)
+    )
+
+}
 
 export default AlpacaForm
