8be08fae1cec8b1c7600769a150ae09bd653970c
Paul Okenne <okennepaul@gmail.com>
2021-02-06 20:25:29 -0500

    feat: add about page information

--- stocky-frontend/src/pages/AboutPage.js
+++ stocky-frontend/src/pages/AboutPage.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const AboutPage=()=>(
     <div id='content'> 
-         <h1>This is the About Page</h1>
+         
+
+        <div className="container" style={{textAlign:'left'}}>
+            <h1>Stocky</h1>
+            <p style={{marginTop:'10px'}}>
+                Stocky is a simple automated stock-trading platform.
+            </p>
+            <p style={{marginTop:'10px'}}>
+
+                Stocky offers various trading strategies that can be implemented on paper trading accounts. At this moment, Stocky supports Long/Short and Mean Reversion strategies.
+            </p>
+
+            <p style={{marginTop:'20px'}}>
+                For more information, please visit the <a target="_blank" href="https://github.com/TheUcheojor/stocky">Stocky Repository </a> 
+            </p>
+
+        </div>
+         
+     
+
     
     </div>
    
