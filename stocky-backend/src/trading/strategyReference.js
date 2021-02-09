3d711cc0a12c607d1b9a23a117e6d8198014f736
Paul Okenne <okennepaul@gmail.com>
2021-02-08 22:58:35 -0500

    refact: add key property to straegy references

--- stocky-backend/src/trading/strategyReference.js
+++ stocky-backend/src/trading/strategyReference.js
@@ -5,8 +5,8 @@ import MeanReversion from './strategies/mean-reversion'
 
 
 const STRATEGIES={
-    LONG_SHORT:{ name: "Long Short", strategyClass: LongShort}, 
-    MEAN_REVERSION:{name: "Mean Reverison",strategyClass: MeanReversion }, 
+    LONG_SHORT:{ key:"LONG_SHORT", name: "Long Short", strategyClass: LongShort}, 
+    MEAN_REVERSION:{key:"MEAN_REVERSION", name: "Mean Reverison",strategyClass: MeanReversion }, 
 }
 
 
