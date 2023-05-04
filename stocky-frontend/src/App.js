786c8a7884e1c1a2325dc6424c2fa473ed153c77
Paul Okenne <okennepaul@gmail.com>
2023-05-04 13:29:10 -0400

    style: update format

--- stocky-frontend/src/App.js
+++ stocky-frontend/src/App.js
@@ -1,66 +1,73 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import logo from "./logo.svg";
 
-import 'sweetalert2/src/sweetalert2.scss'
-import './css/main.css'
-import './css/overview.css'
-import './css/stock-profiles.css'
-import './css/support.css'
-import './css/settings.css'
-import './css/strategy-console.css'
-import './css/login-register.css'
+import "sweetalert2/src/sweetalert2.scss";
+import "./css/main.css";
+import "./css/overview.css";
+import "./css/stock-profiles.css";
+import "./css/support.css";
+import "./css/settings.css";
+import "./css/strategy-console.css";
+import "./css/login-register.css";
 
-import { 
-  BrowserRouter as Router,
-  Route,
-  Switch
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-} from 'react-router-dom'
+import VerticalNavBar from "./components/VerticalNavBar";
+import HorizontalBar from "./components/HorizontalBar";
 
-import VerticalNavBar from './components/VerticalNavBar'
-import HorizontalBar from './components/HorizontalBar'
+import OverviewPage from "./pages/OverviewPage/OverviewPage";
+import AboutPage from "./pages/AboutPage";
+import StockPage from "./pages/StockPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import SettingsPage from "./pages/SettingsPage/SettingsPage";
+import StrategyConsolePage from "./pages/StrategyConsolePage";
 
-import OverviewPage from './pages/OverviewPage/OverviewPage'
-import AboutPage from './pages/AboutPage'
-import StockPage from './pages/StockPage'
-import NotFoundPage from './pages/NotFoundPage'
-import SettingsPage from './pages/SettingsPage/SettingsPage'
-import StrategyConsolePage from './pages/StrategyConsolePage'
+import LoginRegisterPage from "./pages/LoginRegisterPage/LoginRegisterPage";
 
-import LoginRegisterPage from './pages/LoginRegisterPage/LoginRegisterPage'
-
-import useUser from './custom_hooks/useUser'
-import showMobileNavigationMenu from './support/showMobileNavigationMenu'
-
-
-
+import useUser from "./custom_hooks/useUser";
+import showMobileNavigationMenu from "./support/showMobileNavigationMenu";
 
 function App() {
+  const { user, setUser, logoutUser } = useUser();
 
-  const {user, setUser,logoutUser}= useUser();
-
-  if(!user){
-    return <LoginRegisterPage setUser={setUser} />
+  if (!user) {
+    return <LoginRegisterPage setUser={setUser} />;
   }
 
   return (
+    <Router>
+      <div id="header" onClick={showMobileNavigationMenu}>
+        {" "}
+        Stocky{" "}
+      </div>
+      <VerticalNavBar />
+      <HorizontalBar name={user.name} logoutUser={logoutUser} />
+      <Switch>
+        <Route
+          path="/"
+          component={() => <OverviewPage email={user.email} />}
+          exact
+        />
+        <Route path="/about" component={AboutPage} />
+        <Route
+          path="/settings"
+          component={() => <SettingsPage email={user.email} />}
+        />
+        <Route
+          path="/strategy-console"
+          component={() => <StrategyConsolePage email={user.email} />}
+        />
+        <Route
+          path="/stocks/:symbol"
+          component={({ match }) => (
+            <StockPage match={match} email={user.email} />
+          )}
+        />
+        <Route component={NotFoundPage} />
+      </Switch>
 
-      <Router>
-          <div id='header' onClick={showMobileNavigationMenu}> Stocky </div>
-          <VerticalNavBar />
-          <HorizontalBar name={user.name} logoutUser={logoutUser} />
-          <Switch>
-                  <Route  path='/'  component={()=>(<OverviewPage email={user.email} />) }  exact  />
-                  <Route  path='/about'  component={AboutPage}    />
-                  <Route  path='/settings'  component={()=>( <SettingsPage email={user.email} />)}    />
-                  <Route  path='/strategy-console'  component={()=>( <StrategyConsolePage email={user.email} />)}    />
-                  <Route  path='/stocks/:symbol'  component={({match})=>(<StockPage match={match} email={user.email} />)}    />
-                  <Route component={NotFoundPage} />
-          </Switch>
-                    
-          <div id='footer'>Paul Okenne - paulokenne@cmail.carleton.ca</div>
-      </Router>
-       
+      <div id="footer">Paul Okenne - paulokenne@cmail.carleton.ca</div>
+    </Router>
   );
 }
 
